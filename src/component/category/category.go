aa78b98b42b2e6ff1e4ab7076e14554abce9edc5
advancevillage <cugriver@gmail.com>
2019-12-10 13:52:46 +0800

    #richard: SRP 单一责任原则

---
+++ src/component/category/category.go
@@ -0,0 +1,24 @@
+//author: richard
+//@link: https://my.oschina.net/u/2410867/blog/1647801
+package category
+
+const (
+	CategorySchema = "categories"
+)
+
+type CategorizeFactory interface {
+	CreateCategory(category *Category) error
+	DeleteCategory(category *Category) error
+	UpdateCategory(category *Category) error
+}
+
+type Category struct {
+	CategoryId 	   int64  `json:"categoryId,omitempty"`	  		//分类标识
+	CategoryName   string `json:"categoryName,omitempty"`   	//分类名称
+	CategoryStatus int    `json:"categoryStatus,omitempty"`  	//分类状态
+	ChildCategories  []int64 `json:"childCategories,omitempty"`	    //子分类
+	ParentCategories []int64 `json:"parentCategories,omitempty"`	//父分类
+	CategoryCreateTime int64 `json:"categoryCreateTime,omitempty"`	//分类创建时间
+	CategoryUpdateTime int64 `json:"categoryUpdateTime,omitempty"`  //分类修复时间
+	CategoryDeleteTime int64 `json:"categoryDeleteTime,omitempty"`  //分类删除时间
+}
